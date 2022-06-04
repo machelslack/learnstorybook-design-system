f21f5ebe0e3c9b2fd813d1efc8a190def40ab26e
Machel Slack <machel.slack@icloud.com>
2022-06-04 13:14:14 +0100

    removed components

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -1,5 +1,3 @@
-import { css } from 'styled-components';
-
 // Global style variables
 export const background = {
   app: '#F6F9FC',
@@ -77,22 +75,3 @@ export const typography = {
     code: '90',
   },
 };
-
-export const breakpoint = 600;
-export const pageMargin = '5.55555';
-
-export const pageMargins = css`
-  padding: 0 ${spacing.padding.medium}px;
-  @media (min-width: ${breakpoint * 1}px) {
-    margin: 0 ${pageMargin * 1}%;
-  }
-  @media (min-width: ${breakpoint * 2}px) {
-    margin: 0 ${pageMargin * 2}%;
-  }
-  @media (min-width: ${breakpoint * 3}px) {
-    margin: 0 ${pageMargin * 3}%;
-  }
-  @media (min-width: ${breakpoint * 4}px) {
-    margin: 0 ${pageMargin * 4}%;
-  }
-`;
